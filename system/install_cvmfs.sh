469ee2a22d4b9c685cf6f70e2223e45ae85329e2
Valentin Volkl <valentin.volkl@cern.ch>
2025-09-03 16:00:12 +0200

    up

--- system/install_cvmfs.sh
+++ system/install_cvmfs.sh
@@ -3,7 +3,9 @@
 set -e
 
 dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs bash 
-
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs https://research.cs.wisc.edu/htcondor/repo/current/htcondor-release-current.el9.noarch.rpm 
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs condor 
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs cloud-init passwd
 # Adding wlcg and cvmfs repositories
 dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs https://linuxsoft.cern.ch/wlcg/el9/x86_64/wlcg-repo-1.0.0-1.el9.noarch.rpm
 dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm  
@@ -12,6 +14,8 @@ dnf -y --installroot=/ --releasever=/ config-manager --add-repo http://ecsft.cer
 # CernVM config and system
 dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs http://ecsft.cern.ch/dist/cernvm/five/rpms/latest/cernvm-config-default-latest-noarch.rpm
 dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs http://ecsft.cern.ch/dist/cernvm/five/rpms/latest/cernvm-system-default-latest-noarch.rpm
+
+
 
 # Installing patchELF into base layer image
 mkdir -p /tmp_build && cd /tmp_build
